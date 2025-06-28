12fdc167b398f3c59199e7e42974cf9db1b32fc7
liubov belik <vekrisa01@gmail.com>
2025-06-28 23:12:22 +0300

    feat: add user db support

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/mapper/UserRowMapper.java
@@ -0,0 +1,28 @@
+package ru.yandex.practicum.filmorate.storage.mapper;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+public class UserRowMapper implements RowMapper<User> {
+    @Override
+    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+        User user = new User();
+        user.setId(rs.getLong("user_id"));
+        user.setEmail(rs.getString("email"));
+        user.setLogin(rs.getString("login"));
+        user.setName(rs.getString("name"));
+
+        Date birthday = rs.getDate("birthday");
+        if (birthday != null) {
+            user.setBirthday(birthday.toLocalDate());
+        }
+
+        return user;
+    }
+}
