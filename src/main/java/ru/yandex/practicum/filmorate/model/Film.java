f24837e80a7a1e88001ce2d001bceac387f06f8e
liubov belik <vekrisa01@gmail.com>
2025-06-28 18:19:20 +0300

    feat: add new models and do changes to match requirements (new fields, Friendship)

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,18 +1,16 @@
 package ru.yandex.practicum.filmorate.model;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
-import ru.yandex.practicum.filmorate.util.DurationDeserializer;
-import ru.yandex.practicum.filmorate.util.DurationSerializer;
 import ru.yandex.practicum.filmorate.validation.NotBeforeDate;
-import ru.yandex.practicum.filmorate.validation.PositiveDuration;
 
-import java.time.Duration;
 import java.time.LocalDate;
+import java.util.List;
 
 @Data
 public class Film {
@@ -25,8 +23,11 @@ public class Film {
     @NotBeforeDate(EARLIEST_ALLOWED_RELEASE_DATE)
     private LocalDate releaseDate;
     @NotNull
-    @PositiveDuration
-    @JsonSerialize(using = DurationSerializer.class)
-    @JsonDeserialize(using = DurationDeserializer.class)
-    private Duration duration;
+    @Positive
+    private Integer duration;
+    @JsonProperty("mpa")
+    @Valid
+    private Rating rating;
+    @Valid
+    private List<Genre> genres;
 }
