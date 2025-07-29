281ecd244c4087e863edc1c2114c8f9bd65605ca
liubov belik <vekrisa01@gmail.com>
2025-07-29 20:14:56 +0300

    feat: add publishing event to ReviewService

--- src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
@@ -1,12 +1,18 @@
 package ru.yandex.practicum.filmorate.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.yandex.practicum.filmorate.event.ReviewCreatedEvent;
+import ru.yandex.practicum.filmorate.event.ReviewDeletedEvent;
+import ru.yandex.practicum.filmorate.event.ReviewUpdatedEvent;
 import ru.yandex.practicum.filmorate.exception.EntityNotFoundException;
 import ru.yandex.practicum.filmorate.model.Review;
 import ru.yandex.practicum.filmorate.storage.review.ReviewStorage;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Сервис для работы с отзывами о фильмах.
@@ -15,28 +21,42 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ReviewService {
     private final ReviewStorage reviewStorage;
-    private final UserService    userService;
-    private final FilmService    filmService;
+    private final UserService userService;
+    private final FilmService filmService;
+    private final ApplicationEventPublisher publisher;
 
+    @Transactional
     public Review addReview(Review review) {
         userService.getUser(review.getUserId());
         filmService.getFilm(review.getFilmId());
         review.setUseful(0);
-        return reviewStorage.addReview(review);
+        Review addReview = reviewStorage.addReview(review);
+
+        publisher.publishEvent(new ReviewCreatedEvent(addReview.getUserId(), addReview.getReviewId()));
+
+        return addReview;
     }
 
+    @Transactional
     public Review updateReview(Review review) {
         reviewStorage.getReviewById(review.getReviewId())
                 .orElseThrow(() -> new EntityNotFoundException("Отзыв не найден: " + review.getReviewId()));
         userService.getUser(review.getUserId());
         filmService.getFilm(review.getFilmId());
-        return reviewStorage.updateReview(review);
+        Review updateReview = reviewStorage.updateReview(review);
+
+        publisher.publishEvent(new ReviewUpdatedEvent(updateReview.getUserId(), updateReview.getReviewId()));
+        return updateReview;
     }
 
+    @Transactional
     public void deleteReview(Long reviewId) {
-        reviewStorage.getReviewById(reviewId)
-                .orElseThrow(() -> new EntityNotFoundException("Отзыв не найден: " + reviewId));
+        Optional<Review> optionalReview = reviewStorage.getReviewById(reviewId);
+        Review review = optionalReview.orElseThrow(() -> new EntityNotFoundException("Отзыв не найден: " + reviewId));
+
         reviewStorage.deleteReview(reviewId);
+
+        publisher.publishEvent(new ReviewDeletedEvent(review.getUserId(), review.getReviewId()));
     }
 
     public Review getReview(Long reviewId) {
