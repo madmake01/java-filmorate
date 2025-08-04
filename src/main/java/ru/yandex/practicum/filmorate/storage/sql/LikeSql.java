8ff1e96e1f6dc7183585d59e571b1139ed35476f
Sergei-Korshunov <46112607+Sergei-Korshunov@users.noreply.github.com>
2025-08-04 09:57:40 +0300

    feat: most popular
    * Завершил выполнение задания: "Вывод самых популярных фильмов по жанру и годам."
    
    * Добавил модульное тестирование.
    Заменил логику фильтрации (WHERE) в LikeDbStorage.getPopularFilmsWithGenreId.
    
    * CodeStyle:
    1. LikeDbStorageTest - не используемый импорт;
    2. LikeService - в строке 37:82 не поставлен пробел.
    
    * CodeStyle:
    1. LikeSql - модификатор "static" не соответствовал требованиям JLS

--- src/main/java/ru/yandex/practicum/filmorate/storage/sql/LikeSql.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/sql/LikeSql.java
@@ -1,6 +1,29 @@
 package ru.yandex.practicum.filmorate.storage.sql;
 
 public final class LikeSql {
+    public static final  String BASE_QUERY_TO_GET_POPULAR_FILMS = """
+                         SELECT
+                          f.film_id       AS film_id,
+                          f.name          AS film_name,
+                          f.description   AS film_description,
+                          f.release_date  AS film_release_date,
+                          f.duration      AS film_duration,
+                          f.rating_id     AS rating_id,
+                          r.name          AS rating_name,
+                          g.genre_id      AS genre_id,
+                          g.name          AS genre_name,
+                          d.id            AS director_id,
+                          d.name          AS director_name,
+                          COUNT(fl.user_id) AS like_count
+                        FROM films f
+                        JOIN ratings r ON f.rating_id = r.rating_id
+                        LEFT JOIN film_genres fg ON f.film_id = fg.film_id
+                        LEFT JOIN genres g ON fg.genre_id = g.genre_id
+                        LEFT JOIN films_directors AS fd ON fd.film_id=f.film_id
+                        LEFT JOIN directors AS d ON fd.director_id=d.id
+                        LEFT JOIN film_likes AS fl ON fl.film_id=f.film_id
+            """;
+
     public static final String INSERT_LIKE = """
                 INSERT INTO film_likes(user_id, film_id) VALUES (?, ?)
             """;
@@ -12,21 +35,9 @@ public final class LikeSql {
                 FROM film_likes
                 GROUP BY film_id
             """;
-    public static final String SELECT_TOP_FILMS_BY_LIKES = """
-                SELECT
-                            f.film_id,
-                            f.name,
-                            f.description,
-                            f.release_date,
-                            f.duration,
-                            f.rating_id
-                        FROM films f
-                        LEFT JOIN (
-                            SELECT film_id, COUNT(user_id) AS like_count
-                            FROM film_likes
-                            GROUP BY film_id
-                        ) fl ON f.film_id = fl.film_id
-                        ORDER BY fl.like_count DESC
+    public static final String SELECT_TOP_FILMS_BY_LIKES = BASE_QUERY_TO_GET_POPULAR_FILMS + """
+                        GROUP BY f.film_id, g.genre_id
+                        ORDER BY like_count DESC
                         LIMIT ?
             """;
 
